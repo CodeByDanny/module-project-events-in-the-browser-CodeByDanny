e53677c80d76410260cdc6e418189802a6d5f68a
CodeByDanny <1.danny.hamilton@gmail.com>
2023-11-19 11:06:47 -0500

    wip

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@
 function moduleProject2() {
   // 👇 WORK WORK BELOW THIS LINE 👇
   let startTime = new Date().getTime() // Record start time
-
+  
   function getTimeElapsed() { // To be used at end of game to get elapsed time
     let currentTime = new Date().getTime()
     return currentTime - startTime
@@ -37,6 +37,12 @@ function moduleProject2() {
       row.appendChild(square)
       square.addEventListener('click', () => {
         // 👉 TASK 2 - Use a click handler to target a square 👈
+        if(!square.classList.contains('targeted')){
+          getAllSquares().forEach(sq => {
+            sq.classList.remove('targeted')
+          })
+        }
+        square.classList.add('targeted')
       })
     }
   }
